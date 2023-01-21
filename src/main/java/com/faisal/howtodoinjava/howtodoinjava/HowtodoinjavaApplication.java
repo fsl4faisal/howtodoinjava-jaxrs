75039fc44ee77e791ecf427426944fdc115ea9c5
fsl4faisal <fsl4faisal@gmail.com>
2023-01-21 12:13:07 +0530

    second commit

---
+++ src/main/java/com/faisal/howtodoinjava/howtodoinjava/HowtodoinjavaApplication.java
@@ -0,0 +1,13 @@
+package com.faisal.howtodoinjava.howtodoinjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowtodoinjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HowtodoinjavaApplication.class, args);
+	}
+
+}
